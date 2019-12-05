661d6f35165a885120610d4484eaab8d62d38438
ManuelDEV <manueldev250799@gmail.com>
2019-12-05 11:15:26 -0500

    Frontend initial structure

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = (props) => {
+    return (
+      <div>
+        from app
+        
+      </div>
+    );
+}
+
+export default App;
---
+++ frontend/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html")
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: "babel-loader"
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader","css-loader"]
+      },
+      {
+        test: /\.(ttf|woff|eot|jpg|jpeg|svg|png|mp4|mkv|mp3)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "static/"
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    port: 3400,
+    hot: true
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  }
+};
