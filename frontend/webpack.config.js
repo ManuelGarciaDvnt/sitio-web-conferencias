e8608798726e3a1ffbb32e110909c2f1d7950a00
ManuelDEV <manueldev250799@gmail.com>
2019-12-05 12:04:53 -0500

    Backend initial structure

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./frontend/src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].bundle.js"
