ebde1f92131445836d3097f66bb6b74033617ec7
ManuelDEV <manueldev250799@gmail.com>
2019-12-08 11:58:05 -0500

    Counter layout stylished

--- frontend/src/components/Counter/Counter.jsx
+++ frontend/src/components/Counter/Counter.jsx
@@ -3,12 +3,28 @@ import './Counter.css';
 
 const Counter = () => {
     return (
-        <div className="Counter parallax">
-            <div className="container">
-                <div className="e-resume">
-                </div>
+      <div className="Counter parallax relative">
+        <div className="container">
+          <div className="e-resume">
+            <div className="mb-2">
+              <p className="number">9</p>
+              <p>Invitados</p>
             </div>
+            <div className="mb-2">
+              <p className="number">4</p>
+              <p>Talleres</p>
+            </div>
+            <div className="mb-2">
+              <p className="number">9</p>
+              <p>Días</p>
+            </div>
+            <div className="mb-2">
+              <p className="number">9</p>
+              <p>Conferencias</p>
+            </div>
+          </div>
         </div>
+      </div>
     );
 }
 
